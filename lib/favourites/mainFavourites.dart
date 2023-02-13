5484eceb361876ad0db01a57ada9a50b910261f3
shamil <shamil.chahkiev@mail.ru>
2023-02-13 19:06:52 +0300

    Second commit

--- lib/favourites/mainFavourites.dart
+++ lib/favourites/mainFavourites.dart
@@ -3,10 +3,11 @@ import 'Transport.dart';
 import 'Stations.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-  static const List<Tab> myTabs = <Tab>[
+  static const List<Tab> _myTabs = <Tab>[
     Tab(text: 'Transport'),
     Tab(text: 'Stations'),
   ];
+  static const String _title = 'Favourites';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,14 +15,14 @@ class HomePage extends StatelessWidget {
           length: 2,
           child: Scaffold(
               appBar: AppBar(
-                title: new Text('Favourites'),
+                title: new Text(_title),
                 centerTitle: true,
                 bottom: PreferredSize(
                   preferredSize: new Size(16.0, 16.0),
                   child: Container(
                     width: 500.0,
                     child: TabBar(
-                        tabs: myTabs
+                        tabs: _myTabs
                     ),
                   ),
                 ),
--- lib/favourites/transport.dart
+++ lib/favourites/transport.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 class Transport extends StatelessWidget {
   const Transport({Key? key}) : super(key: key);
+  static const String _title = 'Trolleybus-6 Kolbasino - Devyatovka';
+  static const String _description = 'The transport will arrive at 9 10 a.m.';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-        child: Text('Hello'),
-
-      ),
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const ListTile(
+               leading: Icon(Icons.radio_button_checked, color: Colors.deepPurple),
+               title: Text(_title),
+               subtitle: Text(_description),
+    ),
+    ],
+    ),
+    ),
     );
   }
 }
